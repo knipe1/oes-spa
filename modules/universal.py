f0e2a28e24bd15634428ae3674a88a75e09d5545
Wernecke <hauke.wernecke@iaf.fraunhofer.de>
2020-01-20 13:38:00 +0100

    refactoring code Adding unittest and refactor code adding test case for universal.Loadfile outsource of repeated code into methods split classes into two files

--- modules/universal.py
+++ modules/universal.py
@@ -1,3 +1,17 @@
 """This module is for general purposes and includes various functions
 """
 
+from PyQt5.QtWidgets import QFileDialog
+
+# CONFIG
+# allowed file suffixes (seperate by ;;)
+SUFFIXES = "SpexHex File (*.spk);;Exported Raw spectrum (*.csv)";
+
+def LoadFiles(directory):
+    if type(directory) not in [str]:
+        raise TypeError("directory must be a apth (string)")
+    filenames, _ = QFileDialog.getOpenFileNames(
+            caption = 'Load file(s)',
+            directory = directory, 
+            filter = SUFFIXES)
+    return filenames;
