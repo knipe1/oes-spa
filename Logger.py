c5e4619ecfde203d910cf29ce789605ff998f318
= <hauke.wernecke@iaf.fraunhofer.de>
2020-03-31 19:00:59 +0200

    Changing README.md Adding proper docs to Logger.py (to be reviewed) Adding docs Adjusting default logfile to be in the config.yml

--- Logger.py
+++ Logger.py
@@ -12,158 +12,240 @@ Usage:
     # changing config requires access to logger.logger
     logger.logger.setLevel(logging.ERROR) #requires import logging.ERROR
 
+logging levels
+DEBUG — Detailed information, typically of interest only when diagnosing problems.
+INFO — Confirmation that things are working as expected.
+WARNING — An indication that something unexpected happened, or indicative of some problem in the near future (e.g. “disk space low”). The software is still working as expected.
+ERROR — Due to a more serious problem, the software has not been able to perform some function.
+CRITICAL — A serious error, indicating that the program itself may be unable to continue running.
+
+official website
+https://docs.python.org/3/library/logging.html
+
 Created on Sun Mar 29 12:21:03 2020
 
 @author: hauke
-
-TODO: Add metadata
 """
 
-# official website
-# https://docs.python.org/3/library/logging.html
+# TODO: Add metadata
+# TODO: Add streamhandler?
 
 # further descriptive videos
-# https://www.youtube.com/watch?v=g8nQ90Hk328
-# https://www.youtube.com/watch?v=-ARI4Cz-awo
-# https://www.youtube.com/watch?v=jxmzY9soFXg
+# Logging module: https://www.youtube.com/watch?v=g8nQ90Hk328
+# Logging module (simple): https://www.youtube.com/watch?v=-ARI4Cz-awo
+# Logging module (advanced): https://www.youtube.com/watch?v=jxmzY9soFXg
 
 # Advanced techniques
 # logger = logging.getLogger(__name__) --> __name__ is convention
 #   will use another logger with a possible different config to write in 
 #   different files, console output or something else.
-# streamhandler
-# logging.exception
+# streamhandler:
 # splitting of different setting to different handler (files or consoles)
 
 
-
-# logging levels
-# DEBUG — Detailed information, typically of interest only when diagnosing problems.
-# INFO — Confirmation that things are working as expected.
-# WARNING — An indication that something unexpected happened, or indicative of some problem in the near future (e.g. “disk space low”). The software is still working as expected.
-# ERROR — Due to a more serious problem, the software has not been able to perform some function.
-# CRITICAL — A serious error, indicating that the program itself may be unable to continue running.
-
+# standard libs
 import logging
 
-LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-FILENAME = "/home/hauke/IAF/oes-spectra-analysis-Homeversion/debug.log"
-LOG_MODE = "w"
-LOG_LEVEL = logging.DEBUG
-
-
-# # config the logger
-# # getting the (basic) root logger
-# logger = logging.getLogger()
-
-# # setting the debug level
-# logger.setLevel(LOG_LEVEL)
-
-# # removing all handler from previous sessions
-# while len(logger.handlers):
-#     logger.removeHandler(logger.handlers[0])
-
-# # config the default handler
-# fhandler = logging.FileHandler(filename = FILENAME, mode = LOG_MODE)
-# #adding the format
-# formatter = logging.Formatter(LOG_FORMAT)
-# fhandler.setFormatter(formatter)
-# # adding the configured handler
-# logger.addHandler(fhandler)
-
-# # testing the module and its behaviour
-# # logger.debug("Debugging Nachricht")
-# # logger.info("Info Nachricht")
-# # logger.warning("Warnung")
-# # logger.error("Error Nachricht")
-# # logger.critical("Kritische Nachricht")
-
+# local modules/libs
+import modules.Universal as uni
 
 class Logger():
-    def __init__(self, name, file=FILENAME, level = LOG_LEVEL, format = LOG_FORMAT, 
-                 mode = LOG_MODE):
+    """
+    A class used to implement a general logger for each module.
+
+    This class provides a universal logger that can be used for logging into
+    the same file from different modules.
+    
+    Usage: 
+        from Logger import Logger
+        logger = Logger(__name__)
+        # then use logger obj as normal logger for messages
+        logger.debug("debug message")
+        # changing config requires access to logger.logger
+        logger.logger.setLevel(logging.ERROR) #requires import logging.ERROR
+    
+    Attributes
+    ----------
+    level : int
+        The level of Logging (default logging.DEBUG)
+    format : str
+        Entries are formatted according to this format
+        (default %(asctime)s - %(name)s - %(levelname)s - %(message)s).
+    filename : str
+        Logs are written to this file (default defined in config.log->FILE
+                                       ->LOG_FILE)
+    mode : str
+        The logger opens the logfile in this mode. Options are: w, a 
+        (default w). 
+        See also: https://docs.python.org/3/library/functions.html#open
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+    """
+    
+    config = uni.load_config()
+    FILE = config["FILE"]
+    level = logging.DEBUG
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    filename = FILE["LOG_FILE"]
+    mode = "w"
+    
+    def __init__(self, name: str):
+        """
+        Parameters
+        ----------
+        name : str
+            The name used to determine the logger object. Please consider
+            __name__ as common name to distinguish the different entries
+            of the different modules. They are then named in the logs.
+
+        Returns
+        -------
+        None.
+
+        """
         
-        """initialize the parent class ( == logging.Logger.__init__(self)"""
+        # Setting up the logger with the default configuration.
+        self.logger = logging.getLogger(name)   # get or create the logger
+        self.logger.setLevel(self.level)
         
-        # creating a new logger with file specific name
-        self.logger = logging.getLogger(name)
-        
-        # setting the default logging level
-        self.logger.setLevel(logging.DEBUG)
-        
-        
-        # removing all handler from previous sessions
+        # removing all handler from previous sessions to set up a clean logger
         while len(self.logger.handlers):
             self.logger.removeHandler(self.logger.handlers[0])
 
-        # print(self.logger.getEffectiveLevel(), self.logger.handlers)
-        # setting the logging level
-        # try:
-        #     print(self.logger.getEffectiveLevel())
-        #     self.logger.setLevel(logging.DEBUG)
-        # except:
-        #     pass
-        # try:
-        #     print(self.logger.getEffectiveLevel())
-        #     self.logger.setLevel(level)
-        # except:
-        #     pass
-        # try:
-        #     print(self.logger.getEffectiveLevel())
-        #     self.logger.setLevel = level
-        # except:
-        #     pass
-        # try:
-        #     print(self.logger.getEffectiveLevel(), level)
-        # except:
-        #     pass
 
-
-        # setting the format
-        formatter = logging.Formatter(LOG_FORMAT)
         
-        # config the default handler
-        fhandler = logging.FileHandler(filename = FILENAME, mode = LOG_MODE)
-        # setting the logging level
-        fhandler.setLevel(level)
-        #adding the format
+        # config a file handler
+        fhandler = logging.FileHandler(filename = self.filename, 
+                                       mode = self.mode)
+        fhandler.setLevel(self.level)
+        
+        # config the format of the handler
+        formatter = logging.Formatter(self.format)
         fhandler.setFormatter(formatter)
         
-        
-        # # setting the logging level
-        # try:
-        #     print(self.logger.getEffectiveLevel())
-        #     self.logger.setLevel(logging.DEBUG)
-        # except:
-        #     pass
-        # try:
-        #     print(self.logger.getEffectiveLevel())
-        #     self.logger.setLevel(level)
-        # except:
-        #     pass
-        # try:
-        #     print(self.logger.getEffectiveLevel())
-        #     self.logger.setLevel = level
-        # except:
-        #     pass
-        # try:
-        #     print(self.logger.getEffectiveLevel(), level)
-        # except:
-        #     pass
-        
-        # adding the configured handler
+        # the handler can now log messages
         self.logger.addHandler(fhandler)
         
-    def debug(self, msg, extra=None):
+    def debug(self, msg: str, extra=None):
+        """
+        Writing the message to the log with level DEBUG.
+
+        Parameters
+        ----------
+        msg : str
+            This string will appear inside the log.
+        extra : TYPE, optional
+            Additional arguments according to the docs of the logging module. 
+            The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.logger.debug(msg, extra = extra)
-    def info(self, msg, extra=None):
+        
+    def info(self, msg: str, extra=None):
+        """
+        Writing the message to the log with level INFO.
+
+        Parameters
+        ----------
+        msg : str
+            This string will appear inside the log.
+        extra : TYPE, optional
+            Additional arguments according to the docs of the logging module. 
+            The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.logger.info(msg, extra = extra)
-    def warning(self, msg, extra=None):
+        
+    def warning(self, msg: str, extra=None):
+        """
+        Writing the message to the log with level WARNING.
+
+        Parameters
+        ----------
+        msg : str
+            This string will appear inside the log.
+        extra : TYPE, optional
+            Additional arguments according to the docs of the logging module. 
+            The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.logger.warning(msg, extra = extra)
+        
     def error(self, msg, extra=None):
+        """
+        Writing the message to the log with level ERROR.
+
+        Parameters
+        ----------
+        msg : str
+            This string will appear inside the log.
+        extra : TYPE, optional
+            Additional arguments according to the docs of the logging module. 
+            The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.logger.error(msg, extra = extra)
-    def critical(self, msg, extra=None):
+        
+    def critical(self, msg: str, extra=None):
+        """
+        Writing the message to the log with level CRITICAL.
+
+        Parameters
+        ----------
+        msg : str
+            This string will appear inside the log.
+        extra : TYPE, optional
+            Additional arguments according to the docs of the logging module. 
+            The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.logger.critical(msg, extra = extra)
+        
     def exception(self, msg, extra=None):
+        """
+        Writing the message to the log with level ERROR inclusive Traceback.
+
+        Parameters
+        ----------
+        msg : str
+            This string will appear inside the log.
+        extra : TYPE, optional
+            Additional arguments according to the docs of the logging module. 
+            The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.logger.exception(msg, extra = extra)
 
