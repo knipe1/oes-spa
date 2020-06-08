4e5d2c32b9a0a47fb169cbc2230ab107cf129832
hauke.wernecke@merkur.uni-freiburg.de <hauke.wernecke@merkur.uni-freiburg.de>
2020-06-08 13:21:06 +0200

    Fixing the _fitting.yml-examlples. _n-n.yml fittings are not laoded and under development for further releases. Cf. #20.

--- routine_oes_spa.py
+++ routine_oes_spa.py
@@ -39,7 +39,7 @@ def main():
     # automatic open and close routine
     try:
         # window.export_raw()
-        window.window.ddFitting.setCurrentIndex(3)
+        #window.window.ddFitting.setCurrentIndex(3)
         window.file_open("./sample files/Asterix1059 1.Spk")
         # window.file_open("./sample files/SIF/testasc.asc")
         # window.file_open("./sample files/_batch.csv")
@@ -52,21 +52,25 @@ def main():
 
 
     window.batch.show()
+
     # # text = "filename"
     # # arbitrary = THR.Thread(target=emu.key_arbitrary, args=[text])
     # # arbitrary.start()
     # # accept the name
+
     enter = THR.Thread(target=emu.key_accept)
     enter.start()
+
     # # in case of file already exists
     # yes = THR.Thread(target=emu.key_alt_j)
     # yes.start()
-    window.batch.window.btnSetFilename.click()
-    selection = THR.Thread(target=emu.key_select_file, args=[200])
-    selection.start()
-    window.lastdir = window.lastdir+"/Obel276"
-    window.batch.browse_spectra()
-    window.batch.window.btnCalculate.click()
+
+    # window.batch.window.btnSetFilename.click()
+    # selection = THR.Thread(target=emu.key_select_file, args=[200])
+    # selection.start()
+    # window.lastdir = window.lastdir+"/Obel276"
+    # window.batch.browse_spectra()
+    # window.batch.window.btnCalculate.click()
     # window.batch.hide()
 
     sys.exit(app.exec_())
