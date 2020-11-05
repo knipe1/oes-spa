dcf99e8ae9c0ab823e0a3684542a1711cfca710e
Hauke Wernecke <hauke.wernecke@iaf.fraunhofer.de>
2020-11-05 14:34:19 +0100

    Further work in BatchReader.

--- modules/filehandling/filereading/BatchReader.py
+++ modules/filehandling/filereading/BatchReader.py
@@ -21,7 +21,7 @@ import fnmatch
 # FileFramework: base class.
 from modules.filehandling.filereading.FileReader import FileReader
 # specific subReader
-from modules.filehandling.filereading.CsvReader import CsvReader
+from modules.filehandling.filereading.BaseReader import BaseReader
 # further modules
 import modules.Universal as uni
 
@@ -56,6 +56,7 @@ class BatchReader(FileReader):
         # default batch columns must be the value of an CHC.element.
         self.defaultBatchYColumn = CHC.PEAK_AREA.value
         self.defaultBatchXColumn = CHC.HEADER_INFO.value
+        self.nameColumn = CHC.PEAK_NAME.value
 
 
     def read_file(self, **kwargs)->ERR:
@@ -64,58 +65,18 @@ class BatchReader(FileReader):
             # Set up the reader (even if the file is something else than csv, because we use another dialect then).
             fReader = csv.reader(openFile, dialect=self.dialect)
 
-            fileinformation = self.subReader.readout_file(fReader, **kwargs)
+            fileinformation = self.readout_file(fReader, **kwargs)
             self.timeInfo = fileinformation["timeInfo"]
             self.data = fileinformation["data"]
             self.parameter = fileinformation.get("parameter", {})
 
 
-    def readout_file(self, fReader, **kwargs)->dict:
-
+    def read_marker(self, line):
         marker = self.MARKER["HEADER"]
 
-        data = []
-        parameter = {}
-
-        for line in fReader:
-            markerElement, xDataElement, yDataElement = self.get_information(line, **kwargs)
-
-            if self.is_data(xDataElement, yDataElement):
-                pass
-            elif self.contain_marker(marker, markerElement):
-                timeInfo = self.get_time_info(markerElement)
-            else:
-                self.handle_additional_information(markerElement=markerElement, line=line, parameter=parameter, **kwargs)
-
-        try:
-            information = self.join_information(timeInfo, data, parameter)
-        except UnboundLocalError:
-            information = self.join_information(None, None)
-
-        return information
+        markerElement = line[0]
 
 
-    def get_information(self, line)->(str, str, str, str):
-
-        try:
-            markerElement = line[0]
-        except IndexError:
-            # Skip blank lines
-            markerElement = None
-
-        try:
-            xDataElement = line[self.xColumn]
-            yDataElement = line[self.yColumn]
-        except IndexError:
-            xDataElement = None
-            yDataElement = None
-
-        return markerElement, xDataElement, yDataElement
-
-        if (not peakName is None) and (peakName in line):
-            return markerElement, xDataElement, yDataElement, peakName
-        else:
-            return markerElement, xDataElement, yDataElement
 
 
 
