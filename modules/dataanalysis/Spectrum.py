031dd444532f70b940ca0460201ae20bce500c54
Hauke Wernecke <hauke.wernecke@iaf.fraunhofer.de>
2020-12-01 17:35:20 +0100

    Minor bug fix.

--- modules/dataanalysis/Spectrum.py
+++ modules/dataanalysis/Spectrum.py
@@ -155,10 +155,6 @@ class Spectrum():
         """Inits the plots in the ui element regarding e.g. labels."""
         self.reset_plot()
         self.ui.axes.update_layout(**self.labels)
-        import numpy as np; bande = np.loadtxt("./sample files/CH-Peaks2.dat");
-        bande[:,0] = bande[:,0]/10
-        # bande[:,1] = bande[:,1]/100
-        self.ui.axes.plot(bande[:,0], bande[:,1])
 
 
     def reset_plot(self)->None:
