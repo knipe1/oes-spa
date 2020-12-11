5bee84fc24c71c0da35d3c4aab24237553b65a00
Hauke Wernecke <hauke.wernecke@iaf.fraunhofer.de>
2020-12-11 16:47:32 +0100

    Refactor matplotlibwidget.

--- ui/matplotlibwidget.py
+++ ui/matplotlibwidget.py
@@ -5,8 +5,7 @@
 import matplotlib.pyplot as mpl
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT\
-                                            as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Toolbar
 
 # third-party libs
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
@@ -14,27 +13,27 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout
 # local modules/libs
 from ConfigLoader import ConfigLoader
 
-config = ConfigLoader();
-PLOT = config.PLOT
+PLOT = ConfigLoader().PLOT;
+
 
 # set default rc parameter.
-if not PLOT == None:
-    mpl.rc('lines',
-           linewidth=PLOT.get("DEF_LINEWIDTH"),
-           color=PLOT.get("DEF_AXIS_COLOR"))
-    # TODO: config param?
+if PLOT:
+    mpl.rc('lines', linewidth=PLOT.get("DEF_LINEWIDTH"),
+                    color=PLOT.get("DEF_AXIS_COLOR"))
     mpl.rc('font', size=9)
-    # TODO: config param?
     # Formats the axis if data are dates.
     mpl.rc("date.autoformatter", minute="%H:%M")
 
 
 class MplCanvas(Canvas):
-    def __init__(self, parent=None, width=2, height=2, dpi=100):
+    def __init__(self, parent=None, dpi=100)->None:
         # Init a figure before init super, because figure is a arg of super.
-        figure = mpl.Figure(figsize=(width, height), dpi=dpi)
-        self.axes = figure.add_subplot(1, 1, 1)
-        super(MplCanvas, self).__init__(figure)
+        self.figure = mpl.Figure(dpi=dpi)
+        self.axes = self.figure.add_subplot()
+        super().__init__(self.figure)
+
+        # Improves the layout and look of the plots.
+        self.figure.set_tight_layout(True)
 
         # Enable methods to both axes and ui element.
         try:
@@ -42,21 +41,9 @@ class MplCanvas(Canvas):
         except AttributeError as err:
             print(err)
 
-        # TODO: neccessary?
-        # self.setParent(parent)
-        # TODO: neccessary?
-        # docs: https://doc.qt.io/qt-5/qsizepolicy.html
-        # Canvas.setSizePolicy(self, QSizePolicy.Expanding,
-        #                      QSizePolicy.Expanding)
-        # TODO: neccessary?
-        # Canvas.updateGeometry(self)
-
-        # Improves the layout and look of the plots.
-        self.figure.set_tight_layout(True)
-
 
     def update_layout(self, title=None, xLabel=None, yLabel=None, xLimit=None,
-                      yLimit=None, auto=False):
+                      yLimit=None, auto=False)->None:
         """Updates the general layout with (optional) properties."""
 
         axes = self.axes;
@@ -83,15 +70,15 @@ class MplCanvas(Canvas):
 
 
 class MatplotlibWidget(QWidget):
-    def __init__(self, parent):
-        super(MatplotlibWidget, self).__init__()
+    def __init__(self, parent)->None:
+        super().__init__()
         self.vbl = QVBoxLayout()
-        self.add_canvas(self.vbl)
-        self.add_toolbar(self.vbl)
+        self.add_canvas()
+        self.add_toolbar()
         self.setLayout(self.vbl)
 
 
-    def draw(self):
+    def draw(self)->None:
         """Draw the plot immediately."""
         if self.axes.get_legend_handles_labels()[0]:
             self.axes.legend();
@@ -102,18 +89,14 @@ class MatplotlibWidget(QWidget):
         # self.canvas.flush_events()
 
 
-    def add_canvas(self, layout, **kwargs):
-        """Add a Canvas container to the layout."""
-        # A canvas is a container holding several drawing elements(like axis,
-        # lines, shapes, ...)
+    def add_canvas(self, **kwargs)->None:
+        # A canvas is a container holding several drawing elements(like axis, lines, shapes, ...)
         self.canvas = MplCanvas(parent=self, **kwargs)
         self.axes = self.canvas.axes
-        layout.addWidget(self.canvas)
+        self.vbl.addWidget(self.canvas)
 
 
     def add_toolbar(self, layout):
-        """Add a toolbar to the layout."""
-        # Set up a toolbar.
-        mplToolbar = NavigationToolbar(self.canvas, self)
-        layout.addWidget(mplToolbar)
+        mplToolbar = Toolbar(self.canvas, self)
+        self.vbl.addWidget(mplToolbar)
 
