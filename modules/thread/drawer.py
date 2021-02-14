fd0911238bdee2efbca9fd0739448d62a88bbabb
Hauke Wernecke <hauke.wernecke@iaf.fraunhofer.de>
2021-02-14 22:31:13 +0100

    Add Drawer.

---
+++ modules/thread/drawer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 20:58:50 2021
+
+@author: hauke
+"""
+
+
+# standard libs
+
+# third-party libs
+from PyQt5.QtCore import Signal
+
+# local modules/libs
+from modules.thread.Worker import Worker
+
+# constants
+
+
+class Drawer(Worker):
+    signal_filename = Signal(str)
+
+    def run(self):
+        self._obj.plot_spectrum()
+
+
+    def draw(self, obj):
+        self._obj = obj
+        self.start()
